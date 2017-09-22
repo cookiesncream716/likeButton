d78756a438f2b5b1ffcc3dc2263330cc9065ea5c
Heather Walker <cookiesncream@ladle.net>
2017-09-21 17:09:27 -0700

    fixed some bugs

--- likeButton.js
+++ likeButton.js
@@ -11,71 +11,80 @@ registerPlugin(proto(Gem, function(){
 
 	this.build = function(ticket, optionsObservee, api){
 		var that = this
+		this.api = api
 		var text = Text(ticket.subject.title)
 		var likeButton = Image(require('url-loader!./star.png'))
 		var numOfLikes = Text()
-		var whoLiked = Text('')
-		var like = Block(text, likeButton, drop = dropDown(numOfLikes, whoLiked))
+		this.whoLiked = Text('')
+		var like = Block(text, likeButton, drop = dropDown(numOfLikes, this.whoLiked))
 		this.add(like)
 
 		var likesField = optionsObservee.subject.likesField
-		var likers = []
-
-		// getLikersName()
-		var getLikersName = function(){
-			api.User.load(likers).then(function(users){
-				if(likers.length === 0){
-					drop.close()
-					whoLiked.text = ''
-				} else{
-					for(var i=0; i<users.length; i++){
-						whoLiked.text += users[i].displayName() + ', '
-					}
-				}
-			}).done()
-		}
+		this.likers = []
 
 		if(ticket.get(likesField).subject === undefined){
+			console.log('1 - likesField was undefined')
 			numOfLikes.text = 0
-			ticket.set('likes', likers)
+			ticket.set(likesField, this.likers)
+			console.log('1.1 - likesField = ' + ticket.get(likesField).subject)
 		} else{
+			console.log('1.3 - likesField already defined = ')
+			console.log(ticket.get(likesField))
 			// needs tested - how do I put ids in to test and get names
 			numOfLikes.text = ticket.get(likesField).subject.length
+			console.log('1.3.1  - numOfLikes=')
+			// BUG
+			// why is this an object and not text????? 
+			console.log(numOfLikes)
 			for(var i=0; i< ticket.get(likesField).subject.length; i++){
-				likers.push(ticket.get(likesField).subject[i])
+				this.likers.push(ticket.get(likesField).subject[i])
 			}
-			getLikersName()
-		}
-
-		// get current user and see if he is already in likers
-		api.User.current().then(function(user){
-			that.currentUser = user.subject._id
-		}).done()
-		for(var i=0; i<likers.length; i++){
-			if(that.currentUser === likers[i]){
-				likeButton.src = require('url-loader!./star1.png')
+			console.log('1.5 - this.likers = ')
+			console.log(this.likers)
+			if(numOfLikes.text > 0){
+				this.getLikersName()
+			}
+			// get current user and see if already in likers
+			api.User.current().then(function(user){
+				that.currentUser = user.subject._id
+				console.log('1.8 - get currentUser = ' + that.currentUser)
+			}).done()
+			for(var i=0; i<this.likers.length; i++){
+				if(that.currentUser === this.likers[i]){
+					console.log('1.9 - currentUser had already liked')
+					likeButton.src = require('url-loader!./star1.png')
+				}
 			}
 		}
 
 		likeButton.on('click', function(){
+			console.log('2 - clicked')
 			api.User.current().then(function(curUser){ 
 				var curUserIndex = ticket.get(likesField).subject.indexOf(curUser.subject._id)
 				if(curUserIndex === -1){
 					 // user isn't in list so add
-					 likers.push(curUser.subject._id)
-					 ticket.set('likes', likers)
+					 that.likers.push(curUser.subject._id)
+					 ticket.set(likesField, this.likers)
 					 likeButton.src = require('url-loader!./star1.png')
+					 console.log('2.2 - add user to list')
+					 console.log(ticket.get(likesField))
 				} else{
 					// user is in list so remove
-					likers.splice(curUserIndex, 1)
-					ticket.set('likes', likers)
+					that.likers.splice(curUserIndex, 1)
+					ticket.set(likesField, this.likers)
 					likeButton.src = require('url-loader!./star.png')
+					console.log('2.4 - remove user from list')
+					console.log(ticket.get(likesField))
 				}
 			}).done()
 		})
 
 		ticket.get(likesField).on('change', function(){
-			getLikersName()
+			console.log('3 - on change')
+			// BUG
+			// why is ticket.get(likesField).subject undefined???????
+			console.log(ticket.get(likesField).subject)
+			that.getLikersName()
 			numOfLikes.text = ticket.get(likesField).subject.length
 		})
 
@@ -91,6 +100,24 @@ registerPlugin(proto(Gem, function(){
 		// 		border: '1px solid blue'
 		// 	}
 		// })
+	}
+
+	this.getLikersName = function(){
+		var that = this
+		this.api.User.load(this.likers).then(function(users){
+			console.log('1.7 or 3.5 - users = ')
+			console.log(users)
+			console.log(users.length)
+			if(users.length === 0 || users === undefined){
+				drop.close()
+				that.whoLiked.text = ''
+			} else{
+				for(var i=0; i<users.length; i++){
+					console.log(users[i].displayName())
+					that.whoLiked.text += users[i].displayName() + ', '
+				}
+			}
+		}).done()
 	}
 
 	this.getStyle = function(){
