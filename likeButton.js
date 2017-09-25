e5b4b82fd3cd98c7aa86d6d044b9c0a9b3f361d1
Heather Walker <cookiesncream@ladle.net>
2017-09-24 18:13:29 -0700

    fixed bugs

--- likeButton.js
+++ likeButton.js
@@ -20,70 +20,59 @@ registerPlugin(proto(Gem, function(){
 		this.add(like)
 
 		var likesField = optionsObservee.subject.likesField
-		this.likers = []
 
 		if(ticket.get(likesField).subject === undefined){
 			console.log('1 - likesField was undefined')
 			numOfLikes.text = 0
-			ticket.set(likesField, this.likers)
-			console.log('1.1 - likesField = ' + ticket.get(likesField).subject)
+			ticket.set(likesField, [])
+			this.likers = ticket.get(likesField).subject
 		} else{
-			console.log('1.3 - likesField already defined = ')
-			console.log(ticket.get(likesField))
-			// needs tested - how do I put ids in to test and get names
-			numOfLikes.text = ticket.get(likesField).subject.length
-			console.log('1.3.1  - numOfLikes=')
-			// BUG
-			// why is this an object and not text????? 
-			console.log(numOfLikes)
-			for(var i=0; i< ticket.get(likesField).subject.length; i++){
-				this.likers.push(ticket.get(likesField).subject[i])
-			}
-			console.log('1.5 - this.likers = ')
-			console.log(this.likers)
-			if(numOfLikes.text > 0){
-				this.getLikersName()
-			}
+			this.likers = ticket.get(likesField).subject
+			console.log('1.3 - likesField already defined')
+			numOfLikes.text = this.likers.length
+
 			// get current user and see if already in likers
 			api.User.current().then(function(user){
 				that.currentUser = user.subject._id
-				console.log('1.8 - get currentUser = ' + that.currentUser)
 			}).done()
 			for(var i=0; i<this.likers.length; i++){
 				if(that.currentUser === this.likers[i]){
-					console.log('1.9 - currentUser had already liked')
+					console.log('1.9 - currentUser already liked')
 					likeButton.src = require('url-loader!./star1.png')
 				}
+			}
+
+			// get list of names
+			if(ticket.get(likesField).subject.length > 0){
+				this.getLikersName()
 			}
 		}
 
 		likeButton.on('click', function(){
 			console.log('2 - clicked')
-			api.User.current().then(function(curUser){ 
+			api.User.current().then(function(curUser){
 				var curUserIndex = ticket.get(likesField).subject.indexOf(curUser.subject._id)
 				if(curUserIndex === -1){
 					 // user isn't in list so add
 					 that.likers.push(curUser.subject._id)
-					 ticket.set(likesField, this.likers)
+					 ticket.set(likesField, that.likers)
 					 likeButton.src = require('url-loader!./star1.png')
 					 console.log('2.2 - add user to list')
-					 console.log(ticket.get(likesField))
 				} else{
 					// user is in list so remove
 					that.likers.splice(curUserIndex, 1)
-					ticket.set(likesField, this.likers)
+					ticket.set(likesField, that.likers)
 					likeButton.src = require('url-loader!./star.png')
 					console.log('2.4 - remove user from list')
-					console.log(ticket.get(likesField))
+					numOfLikes.text = that.likers.length
+					// not sure if needed if more than 1 liker
+					that.getLikersName()
 				}
 			}).done()
 		})
 
 		ticket.get(likesField).on('change', function(){
 			console.log('3 - on change')
-			// BUG
-			// why is ticket.get(likesField).subject undefined???????
-			console.log(ticket.get(likesField).subject)
 			that.getLikersName()
 			numOfLikes.text = ticket.get(likesField).subject.length
 		})
@@ -95,25 +84,19 @@ registerPlugin(proto(Gem, function(){
 		numOfLikes.on('mouseout', function(){
 			drop.close()
 		})
-		// drop.style = Style({
-		// 	$menu: {
-		// 		border: '1px solid blue'
-		// 	}
-		// })
 	}
 
 	this.getLikersName = function(){
 		var that = this
 		this.api.User.load(this.likers).then(function(users){
 			console.log('1.7 or 3.5 - users = ')
-			console.log(users)
-			console.log(users.length)
 			if(users.length === 0 || users === undefined){
 				drop.close()
 				that.whoLiked.text = ''
+			} else if(users.length === 1){
+				that.whoLiked.text = users[0].displayName()
 			} else{
 				for(var i=0; i<users.length; i++){
-					console.log(users[i].displayName())
 					that.whoLiked.text += users[i].displayName() + ', '
 				}
 			}
