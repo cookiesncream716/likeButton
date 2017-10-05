fd879ebff55aa3dbe3657aaa22c00088c594d943
Heather Walker <cookiesncream@ladle.net>
2017-10-05 09:36:43 -0700

    fixed splice and likesField error

--- likeButton.js
+++ likeButton.js
@@ -24,29 +24,19 @@ registerPlugin(proto(Gem, function(){
 		if(ticket.get(this.likesField).subject === undefined){
 			numOfLikes.text = 0
 			ticket.set(this.likesField, [])
-			// this.likers = ticket.get(likesField).subject
 		} else{
-			// this.likers = ticket.get(likesField).subject
-			// numOfLikes.text = this.likers.length
 			numOfLikes.text = ticket.get(this.likesField).subject.length
-
 			// get current user and see if already in likers
 			api.User.current().then(function(user){
 				that.currentUser = user.subject._id
 			}).done()
-			// for(var i=0; i<this.likers.length; i++){
-			// 	if(that.currentUser === this.likers[i]){
-			// 		likeButton.src = require('url-loader!./star1.png')
-			// 	}
-			// }
-			for(var i=0; i<ticket.get(likesField).subject.length; i++){
+			for(var i=0; i<ticket.get(this.likesField).subject.length; i++){
 				console.log('checking for user')
 				if(that.currentUser === ticket.get(this.likesField).subject[i]){
 					console.log('user in list')
 					likeButton.src = require('url-loader!./star1.png')
 				}
 			}
-
 			// get list of names
 			if(ticket.get(this.likesField).subject.length > 0){
 				this.getLikersName()
@@ -58,20 +48,12 @@ registerPlugin(proto(Gem, function(){
 				var curUserIndex = ticket.get(that.likesField).subject.indexOf(curUser.subject._id)
 				if(curUserIndex === -1){
 					 // user isn't in list so add
-					 // that.likers.push(curUser.subject._id)
-					 // ticket.set(likesField, that.likers)
 					 ticket.get(that.likesField).push(curUser.subject._id)
 					 likeButton.src = require('url-loader!./star1.png')
 				} else{
 					// user is in list so remove
-					// that.likers.splice(curUserIndex, 1)
-					// ticket.set(likesField, that.likers)
-					console.log('index ' + curUserIndex)
-					console.log('before ', ticket.get(that.likesField).subject)
-					ticket.get(that.likesField).subject.splice(curUserIndex, 1)
-					console.log('after ', ticket.get(that.likesField).subject)
+					ticket.get(that.likesField).splice(curUserIndex, 1)
 					likeButton.src = require('url-loader!./star.png')
-					// numOfLikes.text = that.likers.length
 					numOfLikes.text = ticket.get(that.likesField).subject.length
 					// not sure if needed if more than 1 liker
 					that.getLikersName()
@@ -95,7 +77,6 @@ registerPlugin(proto(Gem, function(){
 
 	this.getLikersName = function(){
 		var that = this
-		// this.api.User.load(this.likers).then(function(users){
 		this.api.User.load(this.ticket.get(this.likesField).subject).then(function(users){
 			if(users.length === 0 || users === undefined){
 				drop.close()
--- likeButton.umd.js
+++ likeButton.umd.js
@@ -130,29 +130,19 @@ registerPlugin(proto(Gem, function(){
 		if(ticket.get(this.likesField).subject === undefined){
 			numOfLikes.text = 0
 			ticket.set(this.likesField, [])
-			// this.likers = ticket.get(likesField).subject
 		} else{
-			// this.likers = ticket.get(likesField).subject
-			// numOfLikes.text = this.likers.length
 			numOfLikes.text = ticket.get(this.likesField).subject.length
-
 			// get current user and see if already in likers
 			api.User.current().then(function(user){
 				that.currentUser = user.subject._id
 			}).done()
-			// for(var i=0; i<this.likers.length; i++){
-			// 	if(that.currentUser === this.likers[i]){
-			// 		likeButton.src = require('url-loader!./star1.png')
-			// 	}
-			// }
-			for(var i=0; i<ticket.get(likesField).subject.length; i++){
+			for(var i=0; i<ticket.get(this.likesField).subject.length; i++){
 				console.log('checking for user')
 				if(that.currentUser === ticket.get(this.likesField).subject[i]){
 					console.log('user in list')
 					likeButton.src = __webpack_require__(/*! url-loader!./star1.png */ 1)
 				}
 			}
-
 			// get list of names
 			if(ticket.get(this.likesField).subject.length > 0){
 				this.getLikersName()
@@ -164,20 +154,12 @@ registerPlugin(proto(Gem, function(){
 				var curUserIndex = ticket.get(that.likesField).subject.indexOf(curUser.subject._id)
 				if(curUserIndex === -1){
 					 // user isn't in list so add
-					 // that.likers.push(curUser.subject._id)
-					 // ticket.set(likesField, that.likers)
 					 ticket.get(that.likesField).push(curUser.subject._id)
 					 likeButton.src = __webpack_require__(/*! url-loader!./star1.png */ 1)
 				} else{
 					// user is in list so remove
-					// that.likers.splice(curUserIndex, 1)
-					// ticket.set(likesField, that.likers)
-					console.log('index ' + curUserIndex)
-					console.log('before ', ticket.get(that.likesField).subject)
-					ticket.get(that.likesField).subject.splice(curUserIndex, 1)
-					console.log('after ', ticket.get(that.likesField).subject)
+					ticket.get(that.likesField).splice(curUserIndex, 1)
 					likeButton.src = __webpack_require__(/*! url-loader!./star.png */ 0)
-					// numOfLikes.text = that.likers.length
 					numOfLikes.text = ticket.get(that.likesField).subject.length
 					// not sure if needed if more than 1 liker
 					that.getLikersName()
@@ -201,7 +183,6 @@ registerPlugin(proto(Gem, function(){
 
 	this.getLikersName = function(){
 		var that = this
-		// this.api.User.load(this.likers).then(function(users){
 		this.api.User.load(this.ticket.get(this.likesField).subject).then(function(users){
 			if(users.length === 0 || users === undefined){
 				drop.close()
